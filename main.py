a90f339f93339ae5c9d04c6fa78856dc40e759d1
Лена <lena_kuchina@rambler.ru>
2020-05-23 14:21:15 +0300

    first

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def main():
-    print('Hello')
+from flask import render_template
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    main()
+    app.run(debug=True)
